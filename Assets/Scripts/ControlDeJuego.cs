40bccd8e2642408040348c61a9eaf406404a1a21
Luis Rufián Cruz <lrufian98@gmail.com>
2019-01-22 23:51:33 +0100

    #35

--- Assets/Scripts/ControlDeJuego.cs
+++ Assets/Scripts/ControlDeJuego.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ControlDeJuego : MonoBehaviour
 {
+
+    public static bool nivel1Completo= false;
+    public static bool nivel2Completo = false;
+    public static bool nivel3Completo = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,4 +27,42 @@ public class ControlDeJuego : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+
+    public void CargarPantallaInicio()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void CargarAjustes()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+    public void CargarNivel1()
+    {
+        SceneManager.LoadScene("Nivel_1");
+    }
+
+
+    public void CargarNivel2()
+    {
+        if (nivel1Completo)
+        {
+            SceneManager.LoadScene("Nivel_2");
+        }
+    }
+
+
+    public void CargarNivel3()
+    {
+        if(nivel1Completo && nivel2Completo)
+        {
+            SceneManager.LoadScene("Nivel_3");
+        }
+    }
+
+
 }
