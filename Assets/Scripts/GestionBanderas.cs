94a5bf9a8fdffdf0d4ad711acd0f94b9bf3ea57a
Luis Rufián Cruz <lrufian98@gmail.com>
2019-03-03 13:08:17 +0100

    Arreglos 2
    Marcar Niveles no disponibles

--- Assets/Scripts/GestionBanderas.cs
+++ Assets/Scripts/GestionBanderas.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GestionBanderas : MonoBehaviour
 {
+
+    public Image xNivel2;
+    public Image xNivel3;
+
+
     public GameObject banderaMundo1;
     SpriteRenderer bM1;
     public GameObject banderaMundo2;
@@ -24,6 +30,15 @@ public class GestionBanderas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (ControlDeJuego.nivel1Completo)
+        {
+            xNivel2.color = new Color(0, 0, 0, 0);
+        }
+        if (ControlDeJuego.nivel2Completo)
+        {
+            xNivel3.color = new Color(0, 0, 0, 0);
+        }
+
         bM1.enabled = ControlDeJuego.nivel1Completo;
         bM2.enabled = ControlDeJuego.nivel2Completo;
         bM3.enabled = ControlDeJuego.nivel3Completo;
